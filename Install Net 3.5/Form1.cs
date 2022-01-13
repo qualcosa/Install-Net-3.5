8c8378c4dbbfdf80e90d5881cab0b51c91bd4582
qualcosa <miglordrop@gmail.com>
2022-01-13 03:32:13 +0200

    initial commit

---
+++ Install Net 3.5/Form1.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Diagnostics;
+using System.Globalization;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Install_Net_3._5
+{
+    public partial class Form1 : Form
+    {
+        readonly bool ru = true;
+        public Form1()
+        {
+            InitializeComponent();
+            if (!CultureInfo.CurrentUICulture.ToString().Contains("ru-RU"))
+                ru = false;
+        }
+
+        void Form1_Load(object sender, EventArgs e)
+        {
+            if (MessageBox.Show($"{(ru ? "Начать установку" : "Start installation")} Net 3.5?", "Install Net 3.5", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                Directory.CreateDirectory("C:\\35");
+                File.WriteAllBytes("C:\\35\\microsoft-windows-netfx3-ondemand-package~31bf3856ad364e35~amd64~~.cab", Properties.Resources.microsoft_windows_netfx3_ondemand_package_31bf3856ad364e35_amd64__);
+                Process.Start(new ProcessStartInfo { FileName = "cmd", Arguments = "/c dism /online /enable-feature /featurename:NetFX3 /source:C:\\35 /limitaccess" }).WaitForExit();
+                MessageBox.Show($"{(Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\NET Framework Setup\NDP\v3.5")?.GetValue("Install") != null ? $"Net Framework 3.5 {(ru ? "успешно установлен" : "installed successfully")}" : $"{(ru ? "Что-то пошло не так" : "Something went wrong")}")}", "Install Net 3.5", MessageBoxButtons.OK);
+                Directory.Delete("C:\\35", true);
+                Close();
+            }
+            else Close();
+        }
+    }
+}
---
+++ Install Net 3.5/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных со сборкой.
+[assembly: AssemblyTitle("Install Net 3.5")]
+[assembly: AssemblyDescription("Install Net 3.5")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("qualcosa")]
+[assembly: AssemblyProduct("Install Net 3.5")]
+[assembly: AssemblyCopyright("Copyright © qualcosa")]
+[assembly: AssemblyTrademark("qualcosa")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, следует установить атрибут ComVisible в TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("06872973-f25a-477d-8279-6ff9908aa6df")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
